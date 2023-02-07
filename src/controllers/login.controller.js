f9040ea0dd34d13c42bdf620f7cf1b26ed4d25bd
zs153 <tmp153@gmail.com>
2023-02-07 08:59:50 +0100

    upd

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { createPrivateKey, createHash } from 'crypto'
-import bcrypt from "bcrypt";
+import { createPrivateKey, scrypt } from 'crypto'
 import { V4 } from 'paseto'
 import { puertoAPI, serverAPI, privateKey, secreto } from "../config/settings";
 
@@ -63,35 +62,37 @@ export const autorizar = async (req, res) => {
     usuario = result.data
 
     // verifica contaseña
-    if (compareHashPassword(pwdusu, usuario.PWDUSU)) {
-      const payload = {
-        userid: usuario.USERID,
+    await verify(pwdusu, usuario.PWDUSU, async (err, ret) => {
+      if (err) {
+        throw new Error('No se ha podido verificar la identidad del usuario')
       }
-      const key = createPrivateKey({
-        'key': privateKey,
-        'format': 'pem',
-        'type': 'pkcs8',
-        'cipher': 'aes-256-cbc',
-        'passphrase': secreto,
-      })
-
-      await V4.sign(payload, key, {
-        audience: 'urn:client:claim',
-        issuer: 'http://localhost:4000',
-        expiresIn: '1 minute',
-      }).then(token => {
-        res.writeHead(302, {
-          'Location': `http://${url}/admin/clean/?valid=${token}`,
-          'Content-Type': 'text/plain',
+      if (ret) {
+        const payload = {
+          userid: usuario.USERID,
+        }
+        const key = createPrivateKey({
+          'key': privateKey,
+          'format': 'pem',
+          'type': 'pkcs8',
+          'cipher': 'aes-256-cbc',
+          'passphrase': secreto,
         })
-        res.end()
-      })
-    } else {
-      res.render('log/sign-in', {
-        datos: req.body,
-        alerts: [{ msg: 'La contraseña no es correcta' }]
-      })
-    }
+
+        await V4.sign(payload, key, {
+          audience: 'urn:client:claim',
+          issuer: 'http://localhost:4000',
+          expiresIn: '1 minute',
+        }).then(token => {
+          res.writeHead(302, {
+            'Location': `http://${url}/admin/clean/?valid=${token}`,
+            'Content-Type': 'text/plain',
+          })
+          res.end()
+        })
+      } else {
+        throw new Error('La contraseña no es correcta')        
+      }
+    })
 
     // bcrypt.compare(pwdusu, usuario.PWDUSU, async (err, ret) => {
     //   if (err) {
@@ -135,19 +136,19 @@ export const autorizar = async (req, res) => {
   } catch (err) {
     res.render('log/sign-in', {
       datos: req.body,
-      alerts: [{ msg: 'El usuario no existe' }]
+      alerts: [{ msg: err }]
     })
   }
 }
 export const olvido = async (req, res) => {
   const randomString = Math.random().toString(36).substring(2, 10);
-  const salt = await bcrypt.genSalt(10);
-  const passHash = await bcrypt.hash(randomString, salt);
-  const seed = randomString
+  // const salt = await bcrypt.genSalt(10);
+  //const passHash = await bcrypt.hash(randomString, salt);
+  const passHash = await hash(randomString)
   const context = {
     emausu: req.body.emausu,
     pwdusu: passHash,
-    seed,
+    seed: randomString,
   }
   const datos = {
     url: req.body.url,
@@ -176,20 +177,16 @@ export const change = async (req, res) => {
     const result = await axios.post(`http://${serverAPI}:${puertoAPI}/api/usuario`, {
       usuario,
     });
-
     usuario = result.data
 
     // verifica contaseña
-    bcrypt.compare(pwdact, usuario.PWDUSU, async (err, ret) => {
+    await verify(pwdact, usuario.PWDUSU, async (err,ret) => {
       if (err) {
-        res.render('sign-in', {
-          datos: req.body,
-          alerts: [{ msg: 'No se ha podido verificar la identidad del usuario' }]
-        })
+        throw new Error('No se ha podido verificar la identidad del usuario')
       }
+
       if (ret) {
-        const salt = await bcrypt.genSalt(10);
-        const passHash = await bcrypt.hash(req.body.pwdusu, salt);
+        const passHash = await hash(req.body.pwdusu);
         const context = {
           userid: req.body.userid,
           pwdusu: passHash,
@@ -211,52 +208,59 @@ export const change = async (req, res) => {
           alerts: [{ msg: 'La contraseña no es correcta' }]
         });
       }
-    });
+    })
 
+    // bcrypt.compare(pwdact, usuario.PWDUSU, async (err, ret) => {
+    //   if (err) {
+    //     throw new Error('No se ha podido verificar la identidad del usuario')
+    //   }
+    //   if (ret) {
+    //     const salt = await bcrypt.genSalt(10);
+    //     const passHash = await bcrypt.hash(req.body.pwdusu, salt);
+    //     const context = {
+    //       userid: req.body.userid,
+    //       pwdusu: passHash,
+    //     }
+    //     const datos = {
+    //       url: req.body.url,
+    //     }
+
+    //     await axios.post(`http://${serverAPI}:${puertoAPI}/api/usuarios/change`, {
+    //       context,
+    //     });
+
+    //     res.render("log/okChange", {
+    //       datos
+    //     });
+    //   } else {
+    //     res.render('log/sign-in', {
+    //       datos: req.body,
+    //       alerts: [{ msg: 'La contraseña no es correcta' }]
+    //     });
+    //   }
+    // });
   } catch (error) {
     res.render("log/sign-in", {
       datos: req.body,
-      alerts: [{ msg: 'No se ha podido cambiar la contraseña' }]
+      alerts: [{ msg: error }]
     });
   }
 }
 
 // helpers
-const compareHashPassword = (password, hashedPassword) => {
-  if (hashPasswd(password) === hashedPassword) {
-    return true
-  }
-  return false
-}
-const hashPasswd = (password) => {
-  const salt = crypto.randomBytes(32).toString('hex')
-  return crypto.scryptSync(password, salt, 64, { N: 2048 }).toString('hex')
-}
 async function hash(password) {
   return new Promise((resolve, reject) => {
-    // generate random 16 bytes long salt
-    const salt = crypto.randomBytes(16).toString("hex")
-
-    crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+    scrypt(password, secreto, 64, (err, derivedKey) => {
       if (err) reject(err);
-      resolve(salt + ":" + derivedKey.toString('hex'))
+      resolve(derivedKey.toString('hex'))
     });
   })
 }
 async function verify(password, hash) {
   return new Promise((resolve, reject) => {
-    const [salt, key] = hash.split(":")
-    crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+    scrypt(password, secreto, 64, (err, derivedKey) => {
       if (err) reject(err);
-      resolve(key == derivedKey.toString('hex'))
+      resolve(hash === derivedKey.toString('hex'))
     });
   })
 }
-
-(async function run() {
-  const password1 = await hash("123456")
-  const password2 = await hash("123456")
-  console.log("password1", await verify("123456", password1));
-  console.log("password2", await verify("123456", password2));
-  console.log("password1 == password2", password1 == password2);
-})()
