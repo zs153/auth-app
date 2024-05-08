153ea031501909487da37792a68bb0440f89efb4
zs53 <tmp153@gmail.com>
2024-05-08 10:51:26 +0200

    upd

--- src/services/web-server.js
+++ src/services/web-server.js
@@ -1,14 +1,30 @@
 import http from 'http'
-import path from "path";
+// import path from "path";
 import logger from 'morgan'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { puerto } from '../config/settings'
+import { puertoAPI } from '../config/settings'
 // rutas
 import apiUsuarioRouter from "../routes/usuario.router";
 
 let httpServer
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+};
 
 function initialize() {
   return new Promise((resolve, reject) => {
@@ -16,25 +32,27 @@ function initialize() {
     httpServer = http.createServer(app)
 
     // view engine setup
-    app.set("views", path.join(__dirname, "../views"));
-    app.set("view engine", "ejs");
+    // app.set("views", path.join(__dirname, "../views"));
+    // app.set("view engine", "ejs");
 
     // middleware
     app.use(logger('dev'))
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(cookieParser())
-    app.use(express.static(path.join(__dirname, "../public")));
     app.use(cors())
+    // app.use(express.static(path.join(__dirname, "../public")));
 
     // routes
     app.use("/api", apiUsuarioRouter)
 
+    const port = normalizePort(puertoAPI || "4601");
+
     // server
     httpServer
-      .listen(puerto)
+      .listen(port)
       .on('listening', () => {
-        console.log(`Web server listening on port: ${puerto} `)
+        console.log(`Web server listening on port: ${port} `)
 
         resolve()
       })
